243a483630f253e199f1621de1efde47553f0acc
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-08 14:45:08 +0530

    practicing props with different setup and punchline

---
+++ src/jokes.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Joke(props) {
+    return(
+        <div>
+            <h3>{props.setup}</h3>
+            <p>{props.punchline}</p>
+            <hr/>
+        </div>
+
+    );
+}
+
+export default Joke;
