64fe59038613815bf2743586095290763bc3234e
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-08 14:52:11 +0530

    arrays can also work with curly braces

--- src/App.js
+++ src/App.js
@@ -14,26 +14,35 @@ import Joke from "./jokes";
               <Joke 
                 setup= "Blah bjsbjkcb"
                 punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
+                ispun={false}
+                upvotes={10}
+                downvotes={1000}
+                
             />
             <Joke 
                 setup="I got my daughter a fridge for her birthday." 
                 punchline="I can't wait to see her face light up when she opens it." 
+                ispun={false}
             />
             <Joke 
                 setup="How did the hacker escape the police?" 
                 punchline="He just ransomware!" 
+                ispun={true}
             />
             <Joke 
                 setup="Why don't pirates travel on mountain roads?" 
                 punchline="Scurvy." 
+                ispun={true}
             />
             <Joke 
                 setup="Why do bees stay in the hive in the winter?" 
                 punchline="Swarm." 
+                ispun={true}
             />
             <Joke 
                 setup="What's the best thing about Switzerland?" 
                 punchline="I don't know, but the flag is a big plus!" 
+                ispun={false}
             />
           </div>
     );
