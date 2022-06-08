1512e4707e55458bf036bc85a5b062047f124e3c
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-08 14:57:09 +0530

    header and card modified

--- src/App.js
+++ src/App.js
@@ -5,45 +5,14 @@ import Header from "./navbar";
 import Main from "./hero";
 import Card from "./card";
 import Contact from "./contact";
-import Joke from "./jokes";
+
 
   function App() {
     // Javascript written here
       return(
-          <div className="contacts">
-              <Joke 
-                setup= "Blah bjsbjkcb"
-                punchline="It’s hard to explain puns to kleptomaniacs because they always take things literally."
-                ispun={false}
-                upvotes={10}
-                downvotes={1000}
-                
-            />
-            <Joke 
-                setup="I got my daughter a fridge for her birthday." 
-                punchline="I can't wait to see her face light up when she opens it." 
-                ispun={false}
-            />
-            <Joke 
-                setup="How did the hacker escape the police?" 
-                punchline="He just ransomware!" 
-                ispun={true}
-            />
-            <Joke 
-                setup="Why don't pirates travel on mountain roads?" 
-                punchline="Scurvy." 
-                ispun={true}
-            />
-            <Joke 
-                setup="Why do bees stay in the hive in the winter?" 
-                punchline="Swarm." 
-                ispun={true}
-            />
-            <Joke 
-                setup="What's the best thing about Switzerland?" 
-                punchline="I don't know, but the flag is a big plus!" 
-                ispun={false}
-            />
+          <div>
+            <Header/>
+            <Card/>
           </div>
     );
 }
